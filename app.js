83bf60f4223df714005bc2138e68807d69db109d
Longd612 <longdo.x6xc@gmail.com>
2022-08-24 20:40:49 +0700

    Implement CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xssClean = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +28,12 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) MIDDLEWAREs
+// Implement cors
+app.use(cors());
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
+
 // Serving static file
 
 app.use(express.static(path.join(__dirname, 'public')));
