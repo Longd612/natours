5883d149963e6dd95458649d202c09655c7ceb44
Longd612 <longdo.x6xc@gmail.com>
2022-08-24 15:45:34 +0700

    Prepare app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,10 +9,7 @@ export const bookTour = async (tourId) => {
     const stripe = Stripe(
       'pk_test_51LZzbbB78umF55MTnL7mF8qMXzlfGaSHfbE9v4pFWNWMnmoAbIHJvROEIlB5qq29reOLYhSQ9LqsQQvX8Rq5Mt3t007xUrjcyY'
     );
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
 
     // 2. Create checkout form + charge credit card
     // await stripe.redirectToCheckout({
